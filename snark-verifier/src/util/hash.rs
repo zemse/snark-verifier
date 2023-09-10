a0fa8ec6c27eb0884b9de88b61708f240e138064
Jonathan Wang <31040440+jonathanpwang@users.noreply.github.com>
2023-09-10 14:43:34 -0700

    chore: nit

--- snark-verifier/src/util/hash.rs
+++ snark-verifier/src/util/hash.rs
@@ -3,6 +3,7 @@
 mod poseidon;
 
 pub use crate::util::hash::poseidon::Poseidon;
+#[cfg(feature = "loader_halo2")]
 pub(crate) use halo2_base::poseidon::hasher::spec::OptimizedPoseidonSpec;
 
 #[cfg(feature = "loader_evm")]
